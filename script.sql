eac77be3ebeb432c2aadd0b9e291172f687a332e
FishmasterHM <jxmt089659@163.com>
2017-09-11 16:14:27 +0800

    2017.9.10 afternoon study finished, chapter 14 with extra thinking in script

--- script.sql
+++ script.sql
@@ -1,7 +1,27 @@
-SELECT cust_name, cust_contact
+/*SELECT cust_name, cust_contact
 FROM customers
 WHERE cust_id IN (SELECT DISTINCT cust_id 
       	      	  FROM orders 
 		  WHERE order_num IN (SELECT DISTINCT order_num 
       		    		      FROM orderitems 
-		    		      WHERE item_price >= 10));
+		    		      WHERE item_price >= 10));*/
+
+/*SELECT cust_name, cust_state, (SELECT COUNT(*)
+                               FROM orders
+                               WHERE  orders.cust_id = customers.cust_id) AS orders
+FROM customers;*/
+                                
+/*SELECT prod_id
+FROM orderitems
+WHERE orderitems.order_num = orders.order_num;*/
+
+#查询顾客名字,购买价格大于10的物品的总量
+SELECT cust_name, cust_city, All_sum
+FROM customers, (SELECT orders.cust_id, SUM(a_sum) AS All_sum
+                 FROM (SELECT order_num, SUM(quantity) AS a_sum
+                       FROM orderitems
+                       WHERE item_price >= 10
+                       GROUP BY order_num) AS t1, orders
+                 WHERE t1.order_num = orders.order_num
+                 GROUP BY orders.cust_id) AS t2
+WHERE t2.cust_id = customers.cust_id;
